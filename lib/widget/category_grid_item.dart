2f59a4a9002d459ee9a7708c59ebf2e6a124cbc9
Haroon <moharoon11107@gmail.com>
2024-01-11 12:31:17 +0530

    category screen

---
+++ lib/widget/category_grid_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      borderRadius: BorderRadius.circular(40),
+      splashColor: const Color.fromARGB(178, 255, 18, 2),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 40),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
