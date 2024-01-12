57c677251ae53ce3d92839af1d8468dfd2028d8e
Haroon <moharoon11107@gmail.com>
2024-01-12 15:25:35 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/screens/categories.dart';
+
 import 'package:meals/screens/tabs.dart';
 
 final theme = ThemeData(
